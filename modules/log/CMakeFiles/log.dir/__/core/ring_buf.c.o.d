175ad702697ee7ca4b69019aded5239d746fa57c
joshcoyne <joshcoyne@users.noreply.github.com>
2026-06-25 23:22:34 +0000

    deploy: 01d74d84eafdb6c7c9431ac82495a192ac9b97b8

---
+++ modules/log/CMakeFiles/log.dir/__/core/ring_buf.c.o.d
@@ -0,0 +1,17 @@
+modules/log/CMakeFiles/log.dir/__/core/ring_buf.c.o: \
+  /home/runner/work/engine/engine/modules/core/ring_buf.c \
+  /home/runner/work/engine/engine/modules/core/ring_buf.h \
+  /home/runner/work/_temp/8d4e56ec-5936-4e73-8775-ce89ac28bb71/emsdk-main/upstream/lib/clang/23/include/stddef.h \
+  /home/runner/work/_temp/8d4e56ec-5936-4e73-8775-ce89ac28bb71/emsdk-main/upstream/emscripten/cache/sysroot/include/stddef.h \
+  /home/runner/work/_temp/8d4e56ec-5936-4e73-8775-ce89ac28bb71/emsdk-main/upstream/lib/clang/23/include/__stddef_header_macro.h \
+  /home/runner/work/_temp/8d4e56ec-5936-4e73-8775-ce89ac28bb71/emsdk-main/upstream/lib/clang/23/include/__stddef_ptrdiff_t.h \
+  /home/runner/work/_temp/8d4e56ec-5936-4e73-8775-ce89ac28bb71/emsdk-main/upstream/lib/clang/23/include/__stddef_size_t.h \
+  /home/runner/work/_temp/8d4e56ec-5936-4e73-8775-ce89ac28bb71/emsdk-main/upstream/lib/clang/23/include/__stddef_wchar_t.h \
+  /home/runner/work/_temp/8d4e56ec-5936-4e73-8775-ce89ac28bb71/emsdk-main/upstream/lib/clang/23/include/__stddef_null.h \
+  /home/runner/work/_temp/8d4e56ec-5936-4e73-8775-ce89ac28bb71/emsdk-main/upstream/lib/clang/23/include/__stddef_max_align_t.h \
+  /home/runner/work/_temp/8d4e56ec-5936-4e73-8775-ce89ac28bb71/emsdk-main/upstream/lib/clang/23/include/__stddef_offsetof.h \
+  /home/runner/work/_temp/8d4e56ec-5936-4e73-8775-ce89ac28bb71/emsdk-main/upstream/emscripten/cache/sysroot/include/compat/string.h \
+  /home/runner/work/_temp/8d4e56ec-5936-4e73-8775-ce89ac28bb71/emsdk-main/upstream/emscripten/cache/sysroot/include/string.h \
+  /home/runner/work/_temp/8d4e56ec-5936-4e73-8775-ce89ac28bb71/emsdk-main/upstream/emscripten/cache/sysroot/include/features.h \
+  /home/runner/work/_temp/8d4e56ec-5936-4e73-8775-ce89ac28bb71/emsdk-main/upstream/emscripten/cache/sysroot/include/bits/alltypes.h \
+  /home/runner/work/_temp/8d4e56ec-5936-4e73-8775-ce89ac28bb71/emsdk-main/upstream/emscripten/cache/sysroot/include/strings.h

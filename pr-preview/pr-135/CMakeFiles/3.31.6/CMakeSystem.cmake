b438836a9429f38db59e95034ad03687b6753eaa
joshcoyne <joshcoyne@users.noreply.github.com>
2026-06-26 02:24:57 +0000

    Deploy preview for PR 135 🛫

---
+++ pr-preview/pr-135/CMakeFiles/3.31.6/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-6.17.0-1018-azure")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "6.17.0-1018-azure")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/runner/work/_temp/7c942439-83e4-4cb2-a8de-a1dbe131c8d3/emsdk-main/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake")
+
+set(CMAKE_SYSTEM "Emscripten-1")
+set(CMAKE_SYSTEM_NAME "Emscripten")
+set(CMAKE_SYSTEM_VERSION "1")
+set(CMAKE_SYSTEM_PROCESSOR "x86")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)

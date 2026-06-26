63a28913fb22804f60f0b74e06c9093c8bdc99b6
joshcoyne <joshcoyne@users.noreply.github.com>
2026-06-26 00:48:31 +0000

    Deploy preview for PR 129 🛫

---
+++ pr-preview/pr-129/modules/core/CMakeFiles/subsystem.dir/subsystem.c.o.d
@@ -0,0 +1,3 @@
+modules/core/CMakeFiles/subsystem.dir/subsystem.c.o: \
+  /home/runner/work/engine/engine/modules/core/subsystem.c \
+  /home/runner/work/engine/engine/modules/core/include/subsystem.h
---
+++ pr-preview/pr-129/plugins/hello_plugin/CMakeFiles/hello_plugin.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "../../hello_plugin.wasm"
+  "CMakeFiles/hello_plugin"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/hello_plugin.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
